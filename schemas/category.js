00159a207cd23f01980fc94aced97dc8cb3340b0
KaWuNo <nguyenthuan282003@gmail.com>
2025-04-09 21:48:18 +0700

    start project

---
+++ schemas/category.js
@@ -0,0 +1,17 @@
+let mongoose = require('mongoose');
+
+let categorySchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+    },description:{
+        type:String,
+        default:""
+    },
+    slug:String
+},{
+    timestamps:true
+})
+module.exports = mongoose.model('category',categorySchema)
+// Tao 1 schema cho obj category gồm name,description, timestamp
---
+++ schemas/product.js
@@ -0,0 +1,36 @@
+let mongoose = require('mongoose');
+
+let productSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+    },quantity:{
+        type:Number,
+        min:0,
+        required:true
+    },price:{
+        type:Number,
+        min:0,
+        required:true
+    },description:{
+        type:String,
+        default:""
+    },imgURL:{
+        type:String,
+        default:""
+    },category:{
+        type:mongoose.Types.ObjectId,
+        ref:'category',
+        required:true
+    }
+    ,isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    slug:String
+},{
+    timestamps:true
+})
+module.exports = mongoose.model('product',productSchema)
+// Tao 1 schema cho obj category gồm name,description, timestamp
